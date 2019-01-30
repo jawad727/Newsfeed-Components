0b0c9275fc2caec7503ee68d167d93369d079474
Jawad Hussein <jawaddhussein@gmail.com>
2019-01-30 15:49:59 -0800

    MVP Complete

--- Article/Article.js
+++ Article/Article.js
@@ -14,13 +14,13 @@ class Article {
 
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener("click", e => {
-      expandArticle();
+      this.expandArticle();
     });
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    domElement.classList.toggle('article-open');
+    this.domElement.classList.toggle('article-open');
   }
 }
 
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -14,7 +14,7 @@ const menuButton = document.querySelector(".menu-button");
 // Using your menuButton reference, add a click handler that calls toggleMenu
 
 menuButton.addEventListener('click', e => {
-  toggleMenu();
+  toggleMenu(e);
 })
 
 
